09ec90adedd0e4a02a90965897e17165a5415fcd
Maggie-karuri <karurimargaret111@gmail.com>
2024-03-21 22:32:15 +0300

    scope

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+
+var customerName = 'bob';
+global.bestCustomer = undefined;
+function upperCaseCustomerName() {
+    if (typeof customerName !== 'undefined') {
+        customerName = customerName.toUpperCase();
+    }
+    console.log(upperCaseCustomerName);
+}
+    function setBestCustomer() {
+    bestCustomer = 'not bob';
+    } 
+    function overwriteBestCustomer() {
+     bestCustomer = 'maybe bob'
+    }
+    const leastFavoriteCustomer = 'someone';
+    function changeLeastFavoriteCustomer() {
+        throw new Error('Assignment to constant variable.');
+        console.log(leastFavoriteCustomer);
+}
+console.log(customerName);
+console.log(bestCustomer);
